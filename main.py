0433cb7db7a731d0c4d0875278533b8359c4eb87
FarmerJoe1337 <55901420+FarmerJoe1337@users.noreply.github.com>
2020-11-29 17:41:45 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import time
+import discord
+from discord.ext import commands
+
+client = commands.Bot(".", self_bot = True)
+
+TOKEN = 'your tokens here'
+
+@client.command()
+async def start(ctx):
+    while 1:
+        try:
+            await ctx.send('!d bump')
+            await time.sleep(7220)
+        except:
+            print('err bump failed')
+            
+client.run(TOKEN, bot = False)
